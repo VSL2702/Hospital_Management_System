6f0ed181fd3935570ea718ebb18ea33e73f465b3
VSL2702 <142139380+VSL2702@users.noreply.github.com>
2024-05-05 12:08:38 +0530

    Add files via upload

---
+++ COMMON_APP/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+class CommonAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
+
+class CommonAppConfig(AppConfig):
+    name = 'COMMON_APP'
